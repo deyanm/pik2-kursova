f375943735eaf45c4d8962d39ef0ef7351fabc18
Deyan Marinov <deiandeni13@gmail.com>
2019-05-27 18:31:16 +0300

    Update ListGraves.c

--- source/ListGraves.c
+++ source/ListGraves.c
@@ -1,6 +1,9 @@
 #include "Interface_ListGraves.h"
 
 sNode_t* ListHead = NULL;
+int scanStatus = 0;
+const int monthDays[12] = {31, 28, 31, 30, 31, 30,
+    31, 31, 30, 31, 30, 31};
 
 void MainThread() {
     eStatus_t temp_status = eUninitialized;
@@ -8,12 +11,11 @@ void MainThread() {
     FILE *fp = NULL;
     
     char filename[NAME_SIZE];
-    int status = 0;
-    while (status == 0) {
+    while (scanStatus == 0) {
         printf("Enter text file with data in it, or just the name:\n");
         fgets(filename, NAME_SIZE, stdin);
         if (filename[strlen(filename) - 5] == '.' && filename[strlen(filename) - 4] == 't') {
-            status = 1;
+            scanStatus = 1;
         }
     }
     filename[strlen(filename) - 1] = '\0';
@@ -45,7 +47,8 @@ void showMenu(FILE **fp, const char filename[]) {
         printf("\nChoose option below:\n");
         printf("1 - Add new deceased\n");
         printf("2 - Print all graves records\n");
-        printf("3 - Exit\n");
+        printf("3 - Lookup mourning by unique number\n");
+        printf("4 - Exit & Save\n");
         option = getchar();
         fseek(stdin, 0, SEEK_SET);
         switch (option) {
@@ -65,6 +68,46 @@ void showMenu(FILE **fp, const char filename[]) {
                 break;
             }
             case '3':
+            {
+                long id = 0;
+                scanStatus = 0;
+                while(scanStatus != 1) {
+                    printf("\nEnter unique number: ");
+                    scanStatus = scanf("%ld", &id);
+                    fseek(stdin, 0, SEEK_SET);
+                }
+                sNode_t *found = findUnique(ListHead, id);
+                if (found != NULL) {
+                    time_t t = time(NULL);
+                    struct tm *tm1 = localtime(&t);
+                    tm1->tm_mon += 1;
+                    tm1->tm_year += 1900;
+                    struct tm tm2 = {0};
+                    strptime((found->payload).startDate, "%d-%m-%Y", &tm2);
+                    tm2.tm_mon += 1;
+                    tm2.tm_year += 1900;
+                    long days = getDifference(&tm2, tm1);
+                    switch (days) {
+                        case 40:
+                            printf("\n40-days grief!");
+                            break;
+                        case 182:
+                            printf("\n6-months grief!");
+                            break;
+                        case 365:
+                            printf("\n1-year grief!");
+                            break;
+                        case 1095:
+                            printf("\n3-years grief!");
+                            break;
+                        default:
+                            printf("\nNo grief at the moment!");
+                            break;
+                    }
+                }
+                break;
+            }
+            case '4':
             {
                 fclose(*fp);
                 // because r+ will add the new contents to the end, without trunking the file
@@ -100,32 +143,33 @@ eStatus_t openFile(const char file_path_param[], const char mode[], FILE **fp_pa
 }
 
 eStatus_t readFromFile(FILE *fp_param, sNode_t **head_param) {
-//    sNode_t* temp_node = NULL;
-//    sNode_t* iterator = NULL;
+    //    sNode_t* temp_node = NULL;
+    //    sNode_t* iterator = NULL;
     sGrave_t temp_data = {0};
     
     while (fscanf(fp_param, "%ld | ", &temp_data.uniqueNum) != EOF) {
         
         fscanf(fp_param, "%[^|]s", temp_data.name);
-        fscanf(fp_param, "| %d;%d ", &temp_data.location.row, &temp_data.location.column);
+        fscanf(fp_param, "| %d;%d | ", &temp_data.location.row, &temp_data.location.column);
+        fscanf(fp_param, "%[^|]s", temp_data.startDate);
         fscanf(fp_param, "| %d | ", &temp_data.age);
-        fscanf(fp_param, "%[^\n]s\n", temp_data.date);
+        fscanf(fp_param, "%[^\n]s\n", temp_data.endDate);
         
-//        temp_node = (sNode_t*) calloc(sizeof(sNode_t), 1);
-//        temp_node->payload.uniqueNum = temp_data.uniqueNum;
-//        strcpy(temp_node->payload.name, temp_data.name);
-//        temp_node->payload.age = temp_data.age;
-//        temp_node->payload.location = temp_data.location;
-//        strcpy(temp_node->payload.date, temp_data.date);
-//        temp_node->next = NULL;
+        //        temp_node = (sNode_t*) calloc(sizeof(sNode_t), 1);
+        //        temp_node->payload.uniqueNum = temp_data.uniqueNum;
+        //        strcpy(temp_node->payload.name, temp_data.name);
+        //        temp_node->payload.age = temp_data.age;
+        //        temp_node->payload.location = temp_data.location;
+        //        strcpy(temp_node->payload.date, temp_data.date);
+        //        temp_node->next = NULL;
         
         insert_before(head_param, temp_data);
         
-//        if (*head_param == NULL) {
-//            iterator = *head_param = temp_node;
-//        } else {
-//            iterator = iterator->next = temp_node;
-//        }
+        //        if (*head_param == NULL) {
+        //            iterator = *head_param = temp_node;
+        //        } else {
+        //            iterator = iterator->next = temp_node;
+        //        }
     }
     
     return eSuccess;
@@ -136,7 +180,7 @@ eStatus_t writeToFile(FILE **fp_param, sNode_t *head_param) {
     sNode_t* iterator = head_param;
     
     while (iterator != NULL) {
-        fprintf(*fp_param, "%ld | %s | %d;%d | %d | %s\n", (iterator->payload).uniqueNum, (iterator->payload).name, (iterator->payload).location.row, (iterator->payload).location.column, (iterator->payload).age, (iterator->payload).date);
+        fprintf(*fp_param, "%ld | %s | %d;%d | %s| %d | %s\n", (iterator->payload).uniqueNum, (iterator->payload).name, (iterator->payload).location.row, (iterator->payload).location.column, (iterator->payload).startDate, (iterator->payload).age, (iterator->payload).endDate);
         iterator = iterator->next;
     }
     
@@ -144,12 +188,12 @@ eStatus_t writeToFile(FILE **fp_param, sNode_t *head_param) {
 }
 
 sGrave_t* addNewGrave(void) {
-    int status = 0;
     sGrave_t* grave = (sGrave_t*) malloc(sizeof(sGrave_t));
     long unique = 0;
-    while(status != 1) {
+    scanStatus = 0;
+    while(scanStatus != 1) {
         printf("Unique number: ");
-        status = scanf("%ld", &unique);
+        scanStatus = scanf("%ld", &unique);
         fseek(stdin, 0, SEEK_SET);
     }
     if (findUnique(ListHead, unique) != NULL) {
@@ -160,34 +204,45 @@ sGrave_t* addNewGrave(void) {
     fgets(grave->name, NAME_SIZE, stdin);
     grave->name[strlen(grave->name) - 1] = '\0';
     fseek(stdin, 0, SEEK_SET);
-    status = 0;
-    while(status != 1) {
+    scanStatus = 0;
+    while(scanStatus != 1) {
         printf("Age: ");
-        status = scanf("%d", &grave->age);
+        scanStatus = scanf("%d", &grave->age);
         fseek(stdin, 0, SEEK_SET);
     }
-    status = 0;
-    while(status != 2) {
+    scanStatus = 0;
+    while(scanStatus != 2) {
         printf("Location (row;column): ");
-        status = scanf("%d;%d", &grave->location.row, &grave->location.column);
+        scanStatus = scanf("%d;%d", &grave->location.row, &grave->location.column);
         fseek(stdin, 0, SEEK_SET);
     }
     if (isLocationFree(ListHead, &grave->location) != NULL) {
         printf("\nThis location is not free!");
         return NULL;
     }
-    while (strlen(grave->date) != 11) {
-        printf("End date of rental (\'forever\' or \'dd-mm-yyyy\'): ");
-        fgets(grave->date, NAME_SIZE, stdin);
-        if (strcmp(grave->date, "forever\n") == 0) {
+    
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    strftime(grave->startDate, sizeof(grave->startDate), "%d-%m-%Y", tm);
+    grave->startDate[strlen(grave->startDate)] = '\0';
+    
+    char buff[NAME_SIZE];
+    while (strlen(grave->endDate) != 3) {
+        printf("Enter the years of rental (ex. \'15\' or \'forever\'): ");
+        fgets(buff, NAME_SIZE, stdin);
+        if (strcmp(grave->endDate, "forever\n") == 0) {
+            strcpy(grave->endDate, buff);
             break;
+        } else {
+            int years = atoi(buff);
+            if (years != 0) {
+                tm->tm_year += years;
+                strftime(grave->endDate, sizeof(grave->endDate), "%d-%m-%Y", tm);
+                break;
+            }
         }
     }
-    grave->date[strlen(grave->date) - 1] = '\0';
-    //    time_t t = time(NULL);
-    //    struct tm *tm = localtime(&t);
-    //    strftime(grave->date, sizeof(grave->date), "%d-%m-%Y", tm);
-    //    grave->date[strlen(grave->date)] = '\0';
+    grave->endDate[strlen(grave->endDate)] = '\0';
     
     return grave;
 }
@@ -229,11 +284,11 @@ sNode_t* isLocationFree(sNode_t *head, sLoc_t *loc) {
     while(current != NULL) {
         if ((current->payload).location.row == loc->row
             && (current->payload).location.column == loc->column) {
-            if (strcmp((current->payload).date, "forever") != 0) {
+            if (strcmp((current->payload).endDate, "forever") != 0) {
                 time_t now = time(NULL);
                 struct tm *tm = localtime(&now);
                 struct tm end = {0};
-                strptime((current->payload).date, "%d-%m-%Y", &end);
+                strptime((current->payload).endDate, "%d-%m-%Y", &end);
                 if (tm->tm_year + 1900 < end.tm_year + 1900 && tm->tm_mon + 1 < end.tm_mon + 1 && tm->tm_mday < end.tm_mday) {
                     return current;
                 } else {
@@ -265,10 +320,38 @@ eStatus_t deleteList(sNode_t** head_param) {
 void printData(sNode_t *head_param) {
     sNode_t* iterator = head_param;
     
-    printf("\n   id:\tName:\t\t\t  Age:\tLocation:\t\tEnd date of rental:\n");
-    printf("------------------------------------------------------------------\n");
+    printf("\n   id:\tName:\t\t  Age:\tLocation:\t\tDate created:\t\tEnd date of rental:\n");
+    printf("----------------------------------------------------------------------------------\n");
     while(iterator != NULL) {
-        printf("%5ld\t%-20s%3d%6d;%d%20s\n", (iterator->payload).uniqueNum, (iterator->payload).name,(iterator->payload).age, (iterator->payload).location.row, (iterator->payload).location.column, (iterator->payload).date);
+        printf("%5ld\t%-10s%3d%6d;%d%20s%20s\n", (iterator->payload).uniqueNum, (iterator->payload).name,(iterator->payload).age, (iterator->payload).location.row, (iterator->payload).location.column, (iterator->payload).startDate, (iterator->payload).endDate);
         iterator = iterator->next;
     }
+}
+
+int countLeapYears(struct tm *tm) {
+    int years = tm->tm_year;
+    if (tm->tm_mon <= 2)
+        years--;
+    return years / 4 - years / 100 + years / 400;
+}
+
+long getDifference(struct tm *tm1, struct tm *tm2) {
+    // the tm1 is the date of the death, the tm2 is the current date
+    
+    // initialize count using years and day
+    long n1 = (tm1->tm_year) * 365 + (tm1->tm_mday);
+    
+    // Add days for months in given date
+    for (int i=0; i<(tm1->tm_mon) - 1; i++)
+        n1 += monthDays[i];
+    
+    n1 += countLeapYears(tm1);
+    
+    long n2 = (tm2->tm_year) * 365 + tm2->tm_mday;
+    for (int i=0; i<(tm2->tm_mon) - 1; i++)
+        n2 += monthDays[i];
+    n2 += countLeapYears(tm2);
+    
+    // return difference between two counts
+    return (n2 - n1);
 }
